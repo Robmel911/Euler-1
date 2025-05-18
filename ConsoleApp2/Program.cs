f3778a554478ca7711d0afb659fa3a99c509573a
robme <robme@DESKTOP-3ONHIUK>
2025-05-18 00:02:47 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 0; int p;
+        Console.WriteLine("Ingrese el intervalo");
+        p = int.Parse(Console.ReadLine());
+        int c = p - 1;
+        for (int i=0; i<=c; ++i)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                x = i + x;
+            }
+        }
+        Console.WriteLine("La suma de los multiplos de 3 y 5 es: " + x);
+    }
+}
